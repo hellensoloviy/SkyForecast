8574a4c7d660cde51ec05618c69baf8f199e3fcf
Hellen S <hellensoloviy@gmail.com>
2019-03-18 13:52:14 +0200

    start adding location screen; add launch screen;

---
+++ SkyForecast/Controllers/LocationAuthorizationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LocationAuthorizationViewController.swift
+//  SkyForecast
+//
+//  Created by Hellen Soloviy on 3/18/19.
+//  Copyright © 2019 HellySolovii. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocationAuthorizationViewController: UIViewController {
+    
+    //MARK: - Actions
+    @IBAction func authorizationTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func nonAuthorizedUse(_ sender: UIButton) {
+        
+    }
+    
+}
